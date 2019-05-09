2c7c78a1e15456cedfd49d2d2f32085a68a85479
N Rosser <c3330570@uon.edu.au>
2019-05-09 05:08:04 +0000

    task 2/1 (1 was similar, just a bit more messy)

--- main.c
+++ main.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-int main() {
-float image[5][5][3];
-int x, y;
+
+void pixelMean(float *r, float *g, float *b);
+int main() 
+{
+    float image[5][5][3];
+    int x, y;
 // Don’t change this srand() line!
-srand(0);
-for(y = 0; y < 5; y++) {
-for(x = 0; x < 5; x++) {
-image[x][y][0] = (float)rand()/(float)INT_MAX;
-image[x][y][1] = (float)rand()/(float)INT_MAX;
-image[x][y][2] = (float)rand()/(float)INT_MAX;
-}
-}
-// ----------------------------------------
-// Write your code below this comment block
-// ----------------------------------------
-return 0;
+    srand(0);
+    for(y = 0; y < 5; y++) 
+    {
+        for(x = 0; x < 5; x++) 
+        {
+            image[x][y][0] = (float)rand()/(float)INT_MAX;
+            image[x][y][1] = (float)rand()/(float)INT_MAX;
+            image[x][y][2] = (float)rand()/(float)INT_MAX;
+        }
+    }
+
+    for (y=0; y < 2; y++)
+    {
+        for (x=0; x < 5; x++)
+        {
+           pixelMean(&image[x][y][0], &image[x][y][1], &image[x][y][2]);
+            
+          printf ( "%d %d: %f %f %f\n", x+1, y+1, image[x][y][0], image[x][y][1], image[x][y][2])
+          ;
+        }
+    }
+   
+    
+
+    return 0;
+    }
+void pixelMean(float *r, float *g, float *b)
+{
+    *r= *r + *g + *b;
+    *r= *r/3.0;
+    *g= *r;
+    *b= *g;
+            
 }
