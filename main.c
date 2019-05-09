c80df241f8c38bba576fc90ca8c7871a2ce3bb40
N Rosser <c3330570@uon.edu.au>
2019-05-09 06:48:17 +0000

    Task 3 (incomplete, code doesn't calculate pixelMean correctly)

--- main.c
+++ main.c
@@ -5,40 +5,40 @@
 void pixelMean(float *r, float *g, float *b);
 int main() 
 {
-    float image[5][5][3];
-    int x, y;
-// Don’t change this srand() line!
-    srand(0);
-    for(y = 0; y < 5; y++) 
-    {
-        for(x = 0; x < 5; x++) 
-        {
-            image[x][y][0] = (float)rand()/(float)INT_MAX;
-            image[x][y][1] = (float)rand()/(float)INT_MAX;
-            image[x][y][2] = (float)rand()/(float)INT_MAX;
-        }
-    }
+   int x=0, y=0;
+   float image[x][y][3];
+   
+   FILE *input;
+   input= fopen("RGB.txt", "r"); 
 
-    for (y=0; y < 2; y++)
+    for(y=0; y<2; y++)
     {
-        for (x=0; x < 5; x++)
+        for(x=0; x<5; x++)
         {
-           pixelMean(&image[x][y][0], &image[x][y][1], &image[x][y][2]);
-            
-          printf ( "%d %d: %f %f %f\n", x+1, y+1, image[x][y][0], image[x][y][1], image[x][y][2])
-          ;
+            fscanf(input, "%f %f %f\n", 
+            &image[x][y][0], &image[x][y][1], &image[x][y][2]);
+            printf("%d %d: %f %f %f\n", 
+            x+1, y+1, image[x][y][0], image[x][y][1], image[x][y][2]);
         }
+    }    
+    printf("\n");
+    for (y=0; y<2; y++)
+    {
+        for (x=0; x<5; x++)
+        {
+            pixelMean(&image[x][y][0], &image[x][y][1], &image[x][y][2]);
+           printf("%d %d: %f %f %f\n", 
+            x+1, y+1, image[x][y][0], image[x][y][1], image[x][y][2]);
+        } 
     }
    
-    
-
+    fclose(input);
     return 0;
-    }
+}
 void pixelMean(float *r, float *g, float *b)
 {
     *r= *r + *g + *b;
     *r= *r/3.0;
     *g= *r;
     *b= *g;
-            
 }
